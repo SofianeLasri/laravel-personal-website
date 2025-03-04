3de26413b0a2a29f741afabf04ea2803d39fbff0
SofianeLasri <alasri250@gmail.com>
2025-03-04 16:20:16 +0100

    feat: add method to get optimized pictures in Picture model
    - Introduced `getOptimizedPicture` method to retrieve optimized pictures based on variant and format.
    - Added PHPDoc comments for clarity on method parameters and return type.

--- app/Models/Picture.php
+++ app/Models/Picture.php
@@ -16,6 +16,12 @@ class Picture extends Model
         'size',
     ];
 
+    /**
+     * Get the optimized pictures for the picture.
+     *
+     * @param  string  $variant  thumbnail|small|medium|large|full
+     * @param  string  $format  avif|webp
+     */
     public function getOptimizedPicture(string $variant, string $format): ?OptimizedPicture
     {
         return $this->optimizedPictures->first(function (OptimizedPicture $optimizedPicture) use ($variant, $format) {

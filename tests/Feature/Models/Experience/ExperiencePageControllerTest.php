ebdaa700fdecfe0b122ba92b38aa0371a2655616
SofianeLasri <alasri250@gmail.com>
2025-04-19 22:13:22 +0200

    feat(tests): add unit tests for ExperiencePageController

---
+++ tests/Feature/Models/Experience/ExperiencePageControllerTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature\Models\Experience;
+
+use App\Http\Controllers\Admin\ExperiencePageController;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+#[CoversClass(ExperiencePageController::class)]
+class ExperiencePageControllerTest extends TestCase
+{
+    use RefreshDatabase, WithoutMiddleware;
+
+    #[Test]
+    public function test_list_page(): void
+    {
+        $response = $this->get(route('dashboard.experiences.index'));
+
+        $response->assertOk()
+            ->assertInertia(fn ($page) => $page
+                ->component('dashboard/experiences/List')
+                ->has('experiences')
+            );
+    }
+
+    #[Test]
+    public function test_edit_page(): void
+    {
+        $response = $this->get(route('dashboard.experiences.edit', ['id' => 1]));
+
+        $response->assertOk()
+            ->assertInertia(fn ($page) => $page
+                ->component('dashboard/experiences/Edit')
+                ->has('experience')
+                ->has('technologies')
+            );
+    }
+}

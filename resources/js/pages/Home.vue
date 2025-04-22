1c87b682897fdc057780994c4b6f66117aad6fdd
Sofiane Lasri-Trienpont <alasri250@gmail.com>
2025-04-22 17:01:49 +0200

    feat: add logo component and update Home page layout
    - Introduced a new Logo component to improve the branding on the Home page.
    - Enhanced the Home page layout by adding a navigation bar that includes the logo and developer information.

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import { Head } from '@inertiajs/vue3';
 import Background from '@/components/public/Background.vue';
+import Logo from '@/components/public/Logo.vue';
 </script>
 
 <template>
     <Head title="Accueil" />
     <div class="flex min-h-screen flex-col bg-gray-100">
         <Background />
+        <div class="navbar z-1 flex content-center justify-between py-16">
+            <div class="flex items-center gap-4">
+                <Logo />
+                <div class="flex flex-col">
+                    <div class="text-xl leading-5 font-bold">Sofiane Lasri.</div>
+                    <div class="text-design-system-paragraph-color text-sm leading-3.5">Développeur Full-Stack</div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>

05de37e177271b86994900e445a11d62ae5bc9d2
Sofiane Lasri-Trienpont <alasri250@gmail.com>
2025-04-22 16:49:33 +0200

    feat: add Background component and update Home.vue to use it

---
+++ resources/js/components/public/Background.vue
@@ -0,0 +1,3 @@
+<template>
+    <div class="background absolute top-0 right-0 bottom-0 left-0 bg-[url(/resources/images/public/dots.svg)]"></div>
+</template>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { Head } from '@inertiajs/vue3';
+import Background from '@/components/public/Background.vue';
 </script>
 
 <template>
     <Head title="Accueil" />
     <div class="flex min-h-screen flex-col bg-gray-100">
-        <div class="background absolute top-0 right-0 bottom-0 left-0 bg-[url(/resources/images/public/dots.svg)]"></div>
+        <Background />
     </div>
 </template>

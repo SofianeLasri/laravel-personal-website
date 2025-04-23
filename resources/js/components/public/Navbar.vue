36877ccde3da79a6fa852ddcf5d894da4b7e7962
Sofiane Lasri-Trienpont <alasri250@gmail.com>
2025-04-23 11:56:45 +0200

    feat(components): extract Navbar into a separate component
    - Moved navbar markup from Home.vue to a new Navbar.vue component
    - Added container class and centered content in Home.vue
    - Reordered imports in MarkdownEditor.vue for better organization

---
+++ resources/js/components/public/Navbar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import BarStaggeredRegular from '@/components/font-awesome/BarStaggeredRegular.vue';
+import BlackButton from '@/components/public/BlackButton.vue';
+import NavBrand from '@/components/public/NavBrand.vue';
+import NavSearchBar from '@/components/public/NavSearchBar.vue';
+</script>
+
+<template>
+    <div class="navbar z-1 container flex content-center justify-between py-16">
+        <NavBrand />
+        <div class="flex flex-nowrap items-center justify-center gap-4">
+            <NavSearchBar />
+            <BlackButton>
+                <span>Menu</span>
+                <BarStaggeredRegular class="h-4 fill-white" />
+            </BlackButton>
+        </div>
+    </div>
+</template>
--- resources/js/pages/public/Home.vue
+++ resources/js/pages/public/Home.vue
@@ -1,25 +1,13 @@
 <script setup lang="ts">
-import BarStaggeredRegular from '@/components/font-awesome/BarStaggeredRegular.vue';
 import Background from '@/components/public/Background.vue';
-import BlackButton from '@/components/public/BlackButton.vue';
-import NavBrand from '@/components/public/NavBrand.vue';
-import NavSearchBar from '@/components/public/NavSearchBar.vue';
+import Navbar from '@/components/public/Navbar.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
     <Head title="Accueil" />
-    <div class="flex min-h-screen flex-col bg-gray-100">
+    <div class="flex min-h-screen flex-col items-center bg-gray-100 px-2">
         <Background />
-        <div class="navbar z-1 flex content-center justify-between py-16">
-            <NavBrand />
-            <div class="flex flex-nowrap items-center justify-center gap-4">
-                <NavSearchBar />
-                <BlackButton>
-                    <span>Menu</span>
-                    <BarStaggeredRegular class="h-4 fill-white" />
-                </BlackButton>
-            </div>
-        </div>
+        <Navbar />
     </div>
 </template>

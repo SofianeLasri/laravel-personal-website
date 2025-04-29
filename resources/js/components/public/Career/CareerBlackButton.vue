b4307c22db475985c3520bb5955d12f71156525b
SofianeLasri <alasri250@gmail.com>
2025-04-29 18:18:28 +0200

    fix: prevent logo container from shrinking in career buttons
    Added shrink-0 class to logo container in CareerWhiteButton and CareerBlackButton components to maintain consistent logo size.

--- resources/js/components/public/Career/CareerBlackButton.vue
+++ resources/js/components/public/Career/CareerBlackButton.vue
@@ -10,7 +10,7 @@ const emit = defineEmits(['click']);
 
 <template>
     <div class="flex cursor-pointer items-start justify-start gap-4 self-stretch rounded-2xl bg-black p-2 shadow-lg" @click="emit('click')">
-        <div class="outline-border flex size-20 items-center justify-center rounded-xl bg-white p-2 outline-1">
+        <div class="outline-border flex size-20 shrink-0 items-center justify-center rounded-xl bg-white p-2 outline-1">
             <img class="h-full w-full object-contain" :src="experience.logo || 'https://placehold.co/38x38'" alt="Logo" />
         </div>
         <div class="flex flex-col content-between gap-4">

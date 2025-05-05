ee3489a43944231529600884b3bd03e0f176989f
SofianeLasri <alasri250@gmail.com>
2025-05-05 10:39:03 +0200

    feat(translations): add translations

---
+++ lang/en/home.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'hero' => [
+        'hello' => 'Hello, I am',
+        'developer' => 'Developer',
+        'fullstack' => 'Full-Stack.',
+        'download_cv' => 'Download my resume',
+        'years_experience' => 'Years of experience',
+        'projects_done' => 'Completed projects',
+        'frameworks_mastered' => 'Mastered frameworks',
+    ],
+    'backend' => [
+        'title_part1' => 'Specialized in PHP back-end &',
+        'title_part2' => 'Laravel.',
+        'paragraph1' => 'Developing PHP projects since 2019, if there\'s one area where I excel, it\'s in back-end application development (in PHP).',
+        'paragraph2' => 'That doesn\'t mean I\'m bad at front-end, interfaces have always had an important place in my projects. :)',
+        'paragraph3' => 'But having developed a sort of PHP micro-framework for nearly 2 years, before literally falling in love with Laravel, I think I can legitimately say I specialize in these two technologies.',
+    ],
+    'laravel-section' => [
+        'title' => 'With numerous projects under my belt.',
+        'paragraph' => 'Without having the level of a senior developer as my Laravel certification claims, I master the framework quite well. I invite you to look at the different projects I\'ve created with it; they all have their own little specificity. :)',
+        'view_other_projects' => 'View my other projects',
+    ],
+    'other_skills' => [
+        'title' => 'But I also have other skills!',
+        'paragraph' => 'Generally, I like to dabble in everything. It\'s very important for me to be self-sufficient, regardless of the field.',
+    ],
+    'career' => [
+        'title' => 'My professional & educational background.',
+        'description' => 'Often, it\'s through my personal projects that I learn the most. But my educational and professional background has allowed me to build solid foundations in several areas.',
+    ],
+];

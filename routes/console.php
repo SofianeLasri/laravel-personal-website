35f9f1796b75a3fd4a36a65fcf4db9d527811f3e
SofianeLasri <alasri250@gmail.com>
2025-06-01 17:17:15 +0200

    feat(console): add scheduled commands for processing user agents and IP addresses

--- routes/console.php
+++ routes/console.php
@@ -8,3 +8,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('save:requests')->everyMinute();
+Schedule::command('process:user-agents')->hourly();
+Schedule::command('process:ip-adresses')->everyFiveMinutes();
